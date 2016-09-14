9a06e3f8df05226bbeb04293193192e7b951eb80
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-14 11:25:46 -0600

    add functions for reversing a string

--- hw1.c
+++ hw1.c
@@ -1,27 +1,107 @@
+
+
+#include "hw1.h"
 #include <stdio.h>
+ 
 
-int main(void)
-
-{
-
-char word[100];
-
-int length = 0;
-
-printf("Enter a string: ");
-
-gets(word);
-
-printf(“String before reverse is:%s\n”, word);
-
-while(word[length]!='\0')
+char reverse(char *str, int len)
 
 {
 
-length++;
+int count; int i=0;
+
+char *begin, *last, *k, temp, temp2, *space;
+
+int l=0;
+
+if(len==0)
+
+{
+
+printf("Please enter a string.\n");
+
+return 1;
 
 }
 
-reverse(word,length);
+if(*str=="\0")
+
+{
+
+printf("PLease enter a valid string");
+
+return 1;
 
 }
+
+space=str;
+
+while(i<len)
+
+{
+
+if(*space == ' ')
+
+{
+
+count++;
+
+}
+
+i++;
+
+space++;
+
+}
+
+if(count == len)
+
+{
+
+printf("Please enter a valid string");
+
+return 1;
+
+}
+
+begin=str;
+
+last=str+len-1;
+
+k=str;
+
+while(begin<last)
+
+{
+
+temp=*begin;
+
+*begin=*last;
+
+*last=temp;
+
+last--;
+
+begin++;
+
+l++;
+
+}
+
+printf("Reverse is: ");
+
+while(*k!='\0')
+
+{
+
+printf("%c", *k);
+
+k++;
+
+}
+
+return 0;
+
+
+}
+
--- main.c
+++ main.c
@@ -1,102 +1,29 @@
-#Include “hw.h”
 
- 
-
-char reverse(char *str, int len)
-
-{
-
-int count; int i=0;
-
-char *begin, *last, *k, temp, temp2, *space;
-
-int l=0;
-
-if(len==0)
+#include <stdio.h>
+#include "hw1.h"
+int main(void)
 
 {
 
-printf("Please enter a string.\n");
+char word[100];
 
+int length = 0;
+
+printf("Enter a string: ");
+
+gets(word);
+
+printf("String before reverse is:%s\n", word);
+
+while(word[length]!='\0')
+
+{
+
+length++;
+
+}
+
+reverse(word,length);
 return 1;
 
-}
-
-if(*str=="\0")
-
-{
-
-printf("PLease enter a valid string");
-
-return 1;
-
-}
-
-space=str;
-
-while(i<len)
-
-{
-
-if(*space == ' ')
-
-{
-
-count++;
-
-}
-
-i++;
-
-space++;
-
-}
-
-if(count == len)
-
-{
-
-printf("Please enter a valid string");
-
-return 1;
-
-}
-
-begin=str;
-
-last=str+len-1;
-
-k=str;
-
-while(begin<last)
-
-{
-
-temp=*begin;
-
-*begin=*last;
-
-*last=temp;
-
-last--;
-
-begin++;
-
-l++;
-
-}
-
-printf("Reverse is: ");
-
-while(*k!='\0')
-
-{
-
-printf("%c", *k);
-
-k++;
-
-}
-
-return 0;
 }
