9eee3c7325504147774d9af21e219ed964dd74a0
saka2821@colorado.edu <saka2821@colorado.edu>
2016-09-14 10:39:31 -0600

    add files for reversing a string

---
+++ hw1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+
+char word[100];
+
+int length = 0;
+
+printf("Enter a string: ");
+
+gets(word);
+
+printf(“String before reverse is:%s\n”, word);
+
+while(word[length]!='\0')
+
+{
+
+length++;
+
+}
+
+reverse(word,length);
+
+}
---
+++ hw1.h
@@ -0,0 +1,6 @@
+#ifndef _HW1_H
+
+#define _HW1_H
+
+char reverse(char * str, int len);
+#endif
